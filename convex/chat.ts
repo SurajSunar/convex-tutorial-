f4c34e0f37eac1cebd49a1c9cf3e6b36bd0cdc6a
Suraj Sunar <surbhu06@gmail.com>
2025-10-28 16:36:54 +0600

    app: added feature to save and list via threadId

--- convex/chat.ts
+++ convex/chat.ts
@@ -1,6 +1,18 @@
-import { api, internal } from "./_generated/api";
+import { GoogleGenAI } from "@google/genai";
+import { api, components, internal } from "./_generated/api";
 import { internalAction, mutation, query } from "./_generated/server";
 import { v } from "convex/values";
+import { listMessages, saveMessage } from "@convex-dev/agent";
+import { agent } from "./agent/simple";
+import { paginationOptsValidator } from "convex/server";
+import { authorizeThreadAccess } from "./thread";
+import { da } from "@faker-js/faker";
+
+const GEMINI_API_KEY = process.env.GOOGLE_API_KEY;
+
+const ai = new GoogleGenAI({
+  apiKey: GEMINI_API_KEY,
+});
 
 export const sendMessage = mutation({
   args: {
@@ -21,6 +33,14 @@ export const sendMessage = mutation({
         topic,
       });
     }
+
+    if (args.body.startsWith("/ai")) {
+      // Get the string after the first space
+      const topic = args.body.slice(args.body.indexOf(" ") + 1);
+      await ctx.scheduler.runAfter(0, internal.chat.getGeminiResponse, {
+        topic,
+      });
+    }
   },
 });
 
@@ -38,10 +58,7 @@ export const getWikipediaSummary = internalAction({
   args: { topic: v.string() },
   handler: async (ctx, args) => {
     console.log("wiki 1 =", args.topic);
-    const response = await fetch(
-      "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=" +
-        args.topic
-    );
+    const response = await fetch(process.env.WIKI_API + args.topic);
 
     console.log("wiki 2 =", response);
 
@@ -57,3 +74,66 @@ function getSummaryFromJSON(data: any) {
   const firstPageId = Object.keys(data.query.pages)[0];
   return data.query.pages[firstPageId].extract;
 }
+
+export const getGeminiResponse = internalAction({
+  args: { topic: v.string() },
+  handler: async (ctx, args) => {
+    const response = await ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents: args.topic,
+    });
+
+    await ctx.scheduler.runAfter(0, api.chat.sendMessage, {
+      user: "Gemini",
+      body: response?.text || "",
+    });
+  },
+});
+
+export const sendThreadMessage = mutation({
+  args: { prompt: v.string(), threadId: v.string() },
+  handler: async (ctx, { prompt, threadId }) => {
+    await authorizeThreadAccess(ctx, threadId);
+
+    const { messageId } = await saveMessage(ctx, components.agent, {
+      threadId,
+      prompt,
+    });
+
+    await ctx.scheduler.runAfter(0, internal.chat.generateResponse, {
+      threadId,
+      promptMessageId: messageId,
+    });
+  },
+});
+
+// Generate a response to a user message.
+// Any clients listing the messages will automatically get the new message.
+export const generateResponse = internalAction({
+  args: { promptMessageId: v.string(), threadId: v.string() },
+  handler: async (ctx, { promptMessageId, threadId }) => {
+    const data = await agent.generateText(
+      ctx,
+      { threadId },
+      { promptMessageId }
+    );
+    return data.response.body?.candidates[0].content.parts[0].text;
+  },
+});
+
+export const listThreadMessages = query({
+  args: {
+    threadId: v.string(),
+    paginationOpts: paginationOptsValidator,
+  },
+  handler: async (ctx, args) => {
+    const { threadId, paginationOpts } = args;
+    await authorizeThreadAccess(ctx, threadId);
+    const messages = await listMessages(ctx, components.agent, {
+      threadId,
+      paginationOpts,
+    });
+    // You could add more fields here, join with other tables, etc.
+    return messages;
+  },
+});
