ec4a0bcc71f3784a016879d78c4c6cf16d82d651
Suraj Sunar <surbhu06@gmail.com>
2025-10-29 19:07:25 +0600

    app: added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import Chat from "./Chat";
+import ChatBasic from "./Chatbasic";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 export default function App() {
   return (
-    <main className="chat">
-      <Chat />
-    </main>
+    <>
+      <Router>
+        <nav>
+          <Link to="/">Basic</Link> | <Link to="/chat">Chat</Link>
+        </nav>
+        <main className="chat">
+          <Routes>
+            <Route path="/" element={<ChatBasic />} />
+            <Route path="/chat" element={<Chat />} />
+          </Routes>
+        </main>
+      </Router>
+    </>
   );
 }
