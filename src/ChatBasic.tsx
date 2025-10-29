5c605c0c22c90f4bc4f5a90c36e1b8229a2a0729
Suraj Sunar <surbhu06@gmail.com>
2025-10-29 19:07:01 +0600

    app: created basic chat

---
+++ src/ChatBasic.tsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react";
+import { faker } from "@faker-js/faker";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../convex/_generated/api";
+import { useThread } from "./hooks/useThread";
+
+const NAME = faker.person.fullName();
+
+const ChatBasic = () => {
+  const messages = useQuery(api.chat.getMessages);
+
+  // TODO: Add mutation hook here.
+  const sendMessage = useMutation(api.chat.sendMessage);
+
+  const [newMessageText, setNewMessageText] = useState("");
+
+  useEffect(() => {
+    // Make sure scrollTo works on button click in Chrome
+    setTimeout(() => {
+      window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
+    }, 0);
+  }, [messages]);
+
+  return (
+    <div>
+      <header>
+        <h1>Convex Chat</h1>
+        <p>
+          Connected as <strong>{NAME}</strong>
+        </p>
+      </header>
+      {messages?.reverse().map((message) => (
+        <article
+          key={message._id}
+          className={message.user === NAME ? "message-mine" : ""}
+        >
+          <div>{message.user}</div>
+
+          <p>{message.body}</p>
+        </article>
+      ))}
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          await sendMessage({ user: NAME, body: newMessageText });
+          setNewMessageText("");
+        }}
+      >
+        <input
+          value={newMessageText}
+          onChange={async (e) => {
+            const text = e.target.value;
+            setNewMessageText(text);
+          }}
+          placeholder="Write a message…"
+          autoFocus
+        />
+        <button type="submit" disabled={!newMessageText}>
+          Send
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ChatBasic;
